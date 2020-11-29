923dbd9b1860f904ea57400854e2a15f0fabf160
Seth Williamson <sethjwilliamson@gmail.com>
2020-11-29 16:14:21 -0600

    Update getData.php

--- getData.php
+++ getData.php
@@ -30,34 +30,32 @@
     # Return max if no results found
 	if ($sqlPosts->num_rows > 0) 
 	{
-        $response = $sqlPosts->num_rows;
-        
         $row = mysql_fetch_array($sqlPosts);
         foreach($row as $data) {
             $response = "TEST";
-            $response .= `
-                <div style="position:relative; background: #FFFFFF;border: 1px solid #BBBBBB;border-radius: 4px; width:260px; height:400px; margin:20px">
-                    <div style="position: absolute;width: 100px;height: 24px;right: 0px;top: 0px;background: #C4C4C4;">
-                        <p class="text-center font-medium" style="color: #FFFFFF">
-                            Test
-                        </p>
-                    </div>
-
-                    <div style="position: absolute;height: 218px;left: 20px;right: 20px;top: 25px;background: black;"></div>
-                    <div style="position: absolute;left: 20px;right: 20px;top: 250px;font-family: Inter;font-style: normal;font-weight: normal;font-size: 16px;color: #19191D;">
-                        Leather Shoes Testing Right Here and a second line
-                    </div>
-                    <div style="position: absolute;left: 20px;right: 20px;top: 300px;font-family: Inter;font-style: normal;font-weight: 600;font-size: 16px;color: #19191D;">
-                        Sold by Wrangler
-                    </div>
-                    <div style="position: absolute;height: 35px;left: 20px;right: 20px;top:320px;font-family: Inter;font-style: normal;font-weight: bold;font-size: 24px;line-height: 150%;display: flex;align-items: center;color: rgba(0, 0, 0, 0.87);">
-                        $13.85
-                    </div>
-                    <button style="position: absolute;left:20;width:220;height: 32px;bottom: 10px;background: #FFFFFF;border: 1px solid #9DC2FF;box-sizing: border-box;border-radius: 4px;">
-                        <p class="text-center" style="position: absolute;height: 24px;left: 12px;right: 12px;top: calc(50% - 24px/2);font-family: Inter;font-style: normal;font-weight: 500;font-size: 14px;color: #2264D1;">Add To Cart</p>
-                    </button>
-                </div>
-                `;
+            #$response .= `
+            #    <div style="position:relative; background: #FFFFFF;border: 1px solid #BBBBBB;border-radius: 4px; width:260px; height:400px; margin:20px">
+            #        <div style="position: absolute;width: 100px;height: 24px;right: 0px;top: 0px;background: #C4C4C4;">
+            #            <p class="text-center font-medium" style="color: #FFFFFF">
+            #                Test
+            #            </p>
+            #        </div>
+#
+            #        <div style="position: absolute;height: 218px;left: 20px;right: 20px;top: 25px;background: black;"></div>
+            #        <div style="position: absolute;left: 20px;right: 20px;top: 250px;font-family: Inter;font-style: normal;font-weight: normal;font-size: 16px;color: #19191D;">
+            #            Leather Shoes Testing Right Here and a second line
+            #        </div>
+            #        <div style="position: absolute;left: 20px;right: 20px;top: 300px;font-family: Inter;font-style: normal;font-weight: 600;font-size: 16px;color: #19191D;">
+            #            Sold by Wrangler
+            #        </div>
+            #        <div style="position: absolute;height: 35px;left: 20px;right: 20px;top:320px;font-family: Inter;font-style: normal;font-weight: bold;font-size: 24px;line-height: 150%;display: flex;align-items: center;color: rgba(0, 0, 0, 0.87);">
+            #            $13.85
+            #        </div>
+            #        <button style="position: absolute;left:20;width:220;height: 32px;bottom: 10px;background: #FFFFFF;border: 1px solid #9DC2FF;box-sizing: border-box;border-radius: 4px;">
+            #            <p class="text-center" style="position: absolute;height: 24px;left: 12px;right: 12px;top: calc(50% - 24px/2);font-family: Inter;font-style: normal;font-weight: 500;font-size: 14px;color: #2264D1;">Add To Cart</p>
+            #        </button>
+            #    </div>
+            #    `;
 		}
 		exit($response);
 	} 
