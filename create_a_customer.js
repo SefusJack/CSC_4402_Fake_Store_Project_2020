79b73d9d6f19da99d066e53f362e82299dc481b7
SefusJack <46831414+SefusJack@users.noreply.github.com>
2020-11-29 13:44:03 -0600

    add username and password

--- create_a_customer.js
+++ create_a_customer.js
@@ -2,6 +2,8 @@ var name = "";
 var email = "";
 var address = "";
 var payment_info = "";
+var username = "";
+var password = "";
 
 var mysql = require('mysql');
 
@@ -15,7 +17,7 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  var sql = "INSERT INTO Customer (Name, Email, Address, Payment_Info) VALUES ('" + name + "', '" + email + "', '" + address + "' , '" + payment_info + "');";
+  var sql = "INSERT INTO Customer (Name, Email, Address, Payment_Info) VALUES ('" + name + "', '" + email + "', '" + address + "' , '" + payment_info + "', '" + username + "', '" + password + "');";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Inserted");
--- create_a_vendor.js
+++ create_a_vendor.js
@@ -1,6 +1,8 @@
 var name = "";
 var email = "";
 var address = "";
+var username = "";
+var password = "";
 
 var mysql = require('mysql');
 
@@ -14,7 +16,7 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  var sql = "INSERT INTO Vendor (Vendor_Name, Address, Email) VALUES ('" + name + "', '" + address + "', '" + email + "');";
+  var sql = "INSERT INTO Vendor (Vendor_Name, Address, Email) VALUES ('" + name + "', '" + address + "', '" + email + "', '" + username + "', '" + password + "');";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Inserted");
