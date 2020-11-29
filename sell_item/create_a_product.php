fd7de94178508b13e3c34881e1752e6b61aa22f7
SefusJack <46831414+SefusJack@users.noreply.github.com>
2020-11-29 16:43:29 -0600

    Update create_a_product.php

--- sell_item/create_a_product.php
+++ sell_item/create_a_product.php
@@ -7,7 +7,7 @@ echo "Connected successfully.";
 
 $date = date('Y-m-d H:i:s');
 
-$sql = "INSERT INTO Product (Vendor_ID, Product_Type, Product_Name, Description, Image_URL, Date, Cost, Stock) VALUES ('$_POST[Vendor]', '$_POST[ProductType]', '$_POST[Name]', $_POST[Description]', '$_POST[Image]', '$date', '$_POST[Cost]', '$_POST[Quantity]');";
+$sql = "INSERT INTO Product (Vendor_ID, Product_Type, Product_Name, Description, Image_URL, Date, Cost, Stock) VALUES ('$_POST[Vendor]', '$_POST[ProductType]', '$_POST[Name]', '$_POST[Description]', '$_POST[Image]', '$date', '$_POST[Cost]', '$_POST[Quantity]');";
 if ($con->query($sql)){
   echo " New record is inserted sucessfully.";
 }
