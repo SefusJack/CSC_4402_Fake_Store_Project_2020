2e959bbed9f028d8cc6d24bde89145c0a918a992
SefusJack <46831414+SefusJack@users.noreply.github.com>
2020-11-29 16:40:40 -0600

    Update create_a_product.php

--- sell_item/create_a_product.php
+++ sell_item/create_a_product.php
@@ -5,7 +5,7 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully.";
 
-$date = date("Y/m/d");
+$date = date('Y-m-d H:i:s');
 
 $sql = "INSERT INTO Product (Vendor_ID, Product_Type, Product_Name, Description, Image_URL, Date, Cost, Stock) VALUES ('$_POST[Vendor]', '$_POST[ProductType]', '$_POST[Name]', $_POST[Description]', '$_POST[Image]', '$date', '$_POST[Cost]', '$_POST[Quantity]');";
 if ($con->query($sql)){
