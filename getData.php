1e463c7247cec4e64e649bed62238f65620b2e9b
Seth Williamson <sethjwilliamson@gmail.com>
2020-11-29 17:59:45 -0600

    Update getData.php

--- getData.php
+++ getData.php
@@ -1,35 +1,5 @@
 <?php
-    #error_reporting(E_ALL);
-    #ini_set('display_errors', 'on');
-    # Establish connection to mysql database
 	$conn = new mysqli('localhost', 'store', 'qwerty12qwaszx', "store");
-
-    if (! $conn){
-        throw new Db_Connect_Error(); 
-    }   
-
-	# Get parameters from POST request
-	#$start = $conn->real_escape_string($_POST['start']);
-	#$limit = $conn->real_escape_string($_POST['limit']);
-	#$limit = 2000;
-	#$query = $_POST['query'];
-	
-#	$queryString = 'WHERE ';
-#
-#    # If parameters for times do not exist, use default values
-#	if($query['timeStart'] == "")
-#		$query['timeStart'] = "1111/1/1";
-#	if($query['timeEnd'] == "")
-#		$query['timeEnd'] = date("Y/m/d");
-
-    # Add to query string with time filter
-	#$queryString .= 'products.time BETWEEN "' . $query['timeStart'] . '" AND "' . $query['timeEnd'] . '" ';
-
-	# Query for all posts matching filter criteria
-#	$sqlPosts = $conn->query("SELECT * FROM `posts`
-#		" . $queryString . " 
-#		ORDER BY `time` DESC
-#        LIMIT $start, $limit");
 
     $query = "SELECT * FROM Product";
     
@@ -45,6 +15,6 @@
         return $response;
     }
     else {
-        return $response;
+        return "FALSE";
     }
 ?>
