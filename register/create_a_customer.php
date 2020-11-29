48d799251b39852f5223dd1ecdd950989d961546
SefusJack <46831414+SefusJack@users.noreply.github.com>
2020-11-29 14:45:35 -0600

    oops

--- register/create_a_customer.php
+++ register/create_a_customer.php
@@ -7,8 +7,7 @@ if (!$con)
   die('Could not connect: ' . mysql_error());
   }
 mysql_select_db("db", $con);
-$sql="INSERT INTO nametable (fname, lname) VALUES ('$_POST[fname]','$_POST[lname]')";
-$sql = "INSERT INTO Customer (Name, Email, Address, Payment_Info) VALUES ('$_POST[name]', '$_POST[email]', '$_POST[address]', 'NULL', '$_POST[username]', '$_POST[password]',);";
+$sql = "INSERT INTO Customer (Name, Email, Address, Payment_Info) VALUES ('$_POST[name]', '$_POST[email]', '$_POST[address]', 'NULL', '$_POST[username]', '$_POST[password]');";
 if (!mysql_query($sql,$con))
   {
   die('Error: ' . mysql_error());
