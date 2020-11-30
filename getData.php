1a427230674024f653c0bc1dbfc118ca089841d9
Seth Williamson <sethjwilliamson@gmail.com>
2020-11-29 18:02:34 -0600

    Update getData.php

--- getData.php
+++ getData.php
@@ -9,6 +9,7 @@
 
     # Return max if no results found
     if ($result->num_rows > 0) {
+        $response = "IF";
         while($row = $result->fetch_assoc()) {
             $respone = "TEST";
         }
