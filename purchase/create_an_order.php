ac076822fee1faea800ffbec5613cf8b311b4de1
Seth Williamson <sethjwilliamson@gmail.com>
2020-11-30 16:08:44 -0600

    Update create_an_order.php

--- purchase/create_an_order.php
+++ purchase/create_an_order.php
@@ -5,12 +5,15 @@ if ($con->connect_error) {
 }
 echo "Connected";
 
+if ($con->query("SELECT COUNT(*) FROM Cart WHERE Customer_ID=1") <= 0) {
+  die();
+}
+
 $date = date('Y-m-d H:i:s');
-$sql = "INSERT INTO `Order` (Cost, Date, Address, Customer_ID, Payment_Info) SELECT '0', '$date', '$_POST[customer_address]', '1', '$_POST[customer_payment]' WHERE (SELECT COUNT(*) FROM Cart WHERE Customer_ID=1) > 0;";
+//$sql = "INSERT INTO `Order` (Cost, Date, Address, Customer_ID, Payment_Info) SELECT '0', '$date', '$_POST[customer_address]', '1', '$_POST[customer_payment]' WHERE (SELECT COUNT(*) FROM Cart WHERE Customer_ID=1) > 0;";
+$sql = "INSERT INTO `Order` (Cost, Date, Address, Customer_ID, Payment_Info) VALUES ('0', '$date', '$_POST[customer_address]', '1', '$_POST[customer_payment]');";
 
-error_log($sql, 0);
-
-if ($response = $con->query($sql)){
+if ($con->query($sql)){
   echo "New record is inserted sucessfully";
 }
 else{
@@ -19,8 +22,6 @@ else{
 }
 $order_id = $con->insert_id;
 
-error_log($response, 0);
-error_log($order_id, 0);
 
 $sql = "Update `Order` SET Cost = (SELECT SUM(Quantity * Cost) FROM Cart NATURAL JOIN Product WHERE Customer_ID=1) WHERE Order_ID = '$order_id';";
 if ($con->query($sql)){
