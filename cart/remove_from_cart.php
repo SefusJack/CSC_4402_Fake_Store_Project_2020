d079405712c237d1022cfd0a8fb70a7b72e6d4ae
SefusJack <46831414+SefusJack@users.noreply.github.com>
2020-11-29 19:51:24 -0600

    update cart

--- cart/remove_from_cart.php
+++ cart/remove_from_cart.php
@@ -3,10 +3,10 @@ $con = new mysqli("localhost", "store", "qwerty12qwaszx", "store");
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
-$sql = "INSERT INTO Cart (Customer_ID, Product_ID, Quantity) VALUES ('1' , '$_POST[product_id]', '$_POST[product_quantity]');";
+//echo "Connected successfully";
+$sql = "DELETE FROM Cart WHERE Cart_ID=$_POST[cart_id];";
 if ($con->query($sql)){
-  echo "New record is inserted sucessfully";
+  echo " New record is Deleted sucessfully";
 }
 else{
   echo "Error: ". $sql ."
