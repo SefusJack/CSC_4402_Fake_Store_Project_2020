3adae7c70d49c3adcf245a209c8631210915bd6b
SefusJack <46831414+SefusJack@users.noreply.github.com>
2020-11-30 11:20:31 -0600

    Update add_to_cart.php

--- add_to_cart.php
+++ add_to_cart.php
@@ -4,12 +4,23 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
-$sql = "INSERT INTO Cart (Customer_ID, Product_ID, Quantity) VALUES ('1' , '$_POST[product_id]', '$_POST[product_quantity]');";
-if ($con->query($sql)){
-  echo "New record is inserted sucessfully";
+$sql = "SELECT Product_ID, Quantity FROM Cart WHERE Customer_ID=1 AND Product_ID=$_POST[product_id]";
+$result = $con->query($sql);
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()){
+    $quantity = $row["Quantity"];
+    $quantity = $_POST[product_quantity] + $quantity;
+  }
+  $sql = "UPDATE Cart SET Quantity=$quantity WHERE Customer_ID=1 AND Product_ID=$_POST[product_id]";
 }
 else{
-  echo "Error: ". $sql ."
-". $con->error;
+  $sql = "INSERT INTO Cart (Customer_ID, Product_ID, Quantity) VALUES ('1' , '$_POST[product_id]', '$_POST[product_quantity]');";
+  if ($con->query($sql)){
+    echo "New record is inserted sucessfully";
+  }
+  else{
+    echo "Error: ". $sql ."
+  ". $con->error;
+  }
 }
 ?>
