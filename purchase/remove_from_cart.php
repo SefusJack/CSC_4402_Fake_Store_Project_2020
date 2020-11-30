5908587a73300c979c0b29016054a8b51bb2bfca
SefusJack <46831414+SefusJack@users.noreply.github.com>
2020-11-29 22:07:02 -0600

    test

--- purchase/remove_from_cart.php
+++ purchase/remove_from_cart.php
@@ -6,7 +6,7 @@ if ($conn->connect_error) {
 //echo "Connected successfully";
 $sql = "DELETE FROM Cart WHERE Customer_ID=$_POST[customer_id];";
 if ($con->query($sql)){
-  echo " New record is Deleted sucessfully";
+  //echo " New record is Deleted sucessfully";
 }
 else{
   echo "Error: ". $sql ."
