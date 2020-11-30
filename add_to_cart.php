603c03d7a3272a40d73b0ea196e8cc5be132e6ae
SefusJack <46831414+SefusJack@users.noreply.github.com>
2020-11-29 19:24:44 -0600

    Update add_to_cart.php

--- add_to_cart.php
+++ add_to_cart.php
@@ -6,7 +6,7 @@ if ($conn->connect_error) {
 echo "Connected successfully";
 echo $_POST[product_id];
 echo $_POST[product_quantity];
-$sql = "INSERT INTO Cart (Customer_ID, Product_ID, Quantity) VALUES ('1' , '$_POST[product_id]', ''$POST[product_quantity]');";
+$sql = "INSERT INTO Cart (Customer_ID, Product_ID, Quantity) VALUES ('1' , '$_POST[product_id]', '$POST[product_quantity]');";
 if ($con->query($sql)){
   echo "New record is inserted sucessfully";
 }
