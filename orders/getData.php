9acb3a98b3664c7d3f4f2005e513b5ec10aaa96b
SefusJack <46831414+SefusJack@users.noreply.github.com>
2020-11-30 14:26:18 -0600

    create orders page

---
+++ orders/getData.php
@@ -0,0 +1,60 @@
+<?php
+$servername = "localhost";
+$username = "store";
+$password = "qwerty12qwaszx";
+$dbname = "store";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM `Order` WHERE Customer_ID=1";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $order_id = $row["Order_ID"];
+        $cost = $row["Cost"];
+        $date = $row["Date"];
+        $address = $row["Address"];
+        
+
+
+
+        $sql = "SELECT * FROM Order_Products WHERE Order_ID='$order_id'";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()){
+
+            }
+        }
+
+        echo '
+        <div style="position:relative; background: #FFFFFF;border: 1px solid #BBBBBB;border-radius: 4px; width:260px; height:400px; margin:20px">
+            <div style="position: absolute;width: 100px;height: 24px;right: 0px;top: 0px;background: #C4C4C4;">
+                <p class="text-center font-medium" style="color: #FFFFFF">
+                    ' . $product_type . '
+                </p>
+            </div>
+
+            <div style="position: absolute;height: 218px;left: 20px;right: 20px;top: 25px;background-image: url(\'' . $image_url . '\');background-size: 100%;background-repeat: no-repeat;"></div>
+            <div style="position: absolute;left: 20px;right: 20px;top: 250px;font-family: Inter;font-style: normal;font-weight: normal;font-size: 16px;color: #19191D;">
+                ' . $product_name . '
+            </div>
+            <div style="position: absolute;left: 20px;right: 20px;top: 300px;font-family: Inter;font-style: normal;font-weight: 600;font-size: 16px;color: #19191D;">
+                Sold by ' . $vendor_name . '
+            </div>
+            <div style="position: absolute;height: 35px;left: 20px;right: 20px;top:320px;font-family: Inter;font-style: normal;font-weight: bold;font-size: 24px;line-height: 150%;display: flex;align-items: center;color: rgba(0, 0, 0, 0.87);">
+                $' . $cost . '
+            </div>
+        </div>';
+    }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
