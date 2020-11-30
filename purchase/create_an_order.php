45aab6781b7cca1de9a9273ddc022c72a162669d
SefusJack <46831414+SefusJack@users.noreply.github.com>
2020-11-29 21:59:52 -0600

    add create_an_order

--- purchase/create_an_order.php
+++ purchase/create_an_order.php
@@ -5,7 +5,24 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully";
 
-//$sql = "INSERT INTO Cart (Customer_ID, Product_ID, Quantity) VALUES ('$_POST[Name]', '$_POST[Address]', '$_POST[Email]', $_POST[Username]', '$_POST[Password]');";
+$sql = "SELECT * FROM `Order`;"
+$result = $con->query($sql);
+$order_id = $result->numrows+1;
+
+$sql = "SELECT * FROM Cart WHERE Customer_ID=1;"
+$result = $con->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+      $product_id = $row["Product_ID"];
+      $sql = "INSERT INTO Order_Products (Order_ID, Product_ID) VALUES ('$order_id', '$product_id');";
+  }
+}
+
+$date = date('Y-m-d H:i:s');
+
+$sql = "INSERT INTO Order (Product_ID, Cost, Date, Address, Customer_ID, Payment_Info) VALUES ('NULL', '100', '$date', $_POST[Address]', '1');";
 if ($con->query($sql)){
   echo "New record is inserted sucessfully";
 }
