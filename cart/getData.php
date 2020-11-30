f29797555e7fd9297e24e95f8a34158c094f4f37
SefusJack <46831414+SefusJack@users.noreply.github.com>
2020-11-29 18:04:50 -0600

    update

---
+++ cart/getData.php
@@ -0,0 +1,7 @@
+<?php
+$con = new mysqli("localhost", "store", "qwerty12qwaszx", "store");
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully.";
+?>
