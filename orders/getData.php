808320efe545f19fa6276d9c51ac5549d324e8ff
SefusJack <46831414+SefusJack@users.noreply.github.com>
2020-11-30 14:39:04 -0600

    Update getData.php

--- orders/getData.php
+++ orders/getData.php
@@ -22,7 +22,6 @@ if ($result->num_rows > 0) {
         $date = $row["Date"];
         $address = $row["Address"];
         
-        echo '<div style="position:relative; background: #FFFFFF;border: 1px solid #BBBBBB;border-radius: 4px; width:260px; height:400px; margin:20px">';
 
         $sql = "SELECT * FROM Order_Products NATURAL JOIN Product WHERE Order_ID='$order_id'";
         $result = $conn->query($sql);
@@ -36,28 +35,29 @@ if ($result->num_rows > 0) {
                 $quantity = $row["Quantity"];
 
                 echo '
-                <div style="position: absolute;width: 50px;height: 24px;left: 0px;top: 0px;background: #C4C4C4; border-bottom-right-radius: 15px;">
-                    <p class="text-center font-medium" style="color: #FFFFFF">
-                        ' . $quantity . '
-                    </p>
-                </div>
-                <div style="position: absolute;width: 100px;height: 24px;right: 0px;top: 0px;background: #C4C4C4; border-bottom-left-radius: 15px;">
-                    <p class="text-center font-medium" style="color: #FFFFFF">
-                        ' . $product_type . '
-                    </p>
-                </div>
-
-                <div style="position: absolute;height: 218px;left: 20px;right: 20px;top: 25px;background-image: url(\'' . $image_url . '\');background-size: 100%;background-repeat: no-repeat;"></div>
-                <div style="position: absolute;left: 20px;right: 20px;top: 250px;font-family: Inter;font-style: normal;font-weight: normal;font-size: 16px;color: #19191D;">
-                    ' . $product_name . '
-                </div>
-                <div style="position: absolute;left: 20px;right: 20px;top: 300px;font-family: Inter;font-style: normal;font-weight: 600;font-size: 16px;color: #19191D;">
-                    Sold by ' . $vendor_name . '
-                </div>
-                <div style="position: absolute;height: 35px;left: 20px;right: 20px;top:320px;font-family: Inter;font-style: normal;font-weight: bold;font-size: 24px;line-height: 150%;display: flex;align-items: center;color: rgba(0, 0, 0, 0.87);">
-                    $' . $cost . '
-                </div>
-            </div>';
+                <div style="position:relative; background: #FFFFFF;border: 1px solid #BBBBBB;border-radius: 4px; width:260px; height:400px; margin:20px">
+                    <div style="position: absolute;width: 50px;height: 24px;left: 0px;top: 0px;background: #C4C4C4; border-bottom-right-radius: 15px;">
+                        <p class="text-center font-medium" style="color: #FFFFFF">
+                            ' . $quantity . '
+                        </p>
+                    </div>
+                    <div style="position: absolute;width: 100px;height: 24px;right: 0px;top: 0px;background: #C4C4C4; border-bottom-left-radius: 15px;">
+                        <p class="text-center font-medium" style="color: #FFFFFF">
+                            ' . $product_type . '
+                        </p>
+                    </div>
+        
+                    <div style="position: absolute;height: 218px;left: 20px;right: 20px;top: 25px;background-image: url(\'' . $image_url . '\');background-size: 100%;background-repeat: no-repeat;"></div>
+                    <div style="position: absolute;left: 20px;right: 20px;top: 250px;font-family: Inter;font-style: normal;font-weight: normal;font-size: 16px;color: #19191D;">
+                        ' . $product_name . '
+                    </div>
+                    <div style="position: absolute;left: 20px;right: 20px;top: 300px;font-family: Inter;font-style: normal;font-weight: 600;font-size: 16px;color: #19191D;">
+                        Sold by ' . $vendor_name . '
+                    </div>
+                    <div style="position: absolute;height: 35px;left: 20px;right: 20px;top:320px;font-family: Inter;font-style: normal;font-weight: bold;font-size: 24px;line-height: 150%;display: flex;align-items: center;color: rgba(0, 0, 0, 0.87);">
+                        $' . $cost . '
+                    </div>
+                </div>';
             }
         }
     }
