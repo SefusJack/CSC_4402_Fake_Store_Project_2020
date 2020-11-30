f9e083ea9a93ebd76ef26a5508b2395bd4143632
Seth Williamson <sethjwilliamson@gmail.com>
2020-11-30 15:50:31 -0600

    Update create_an_order.php

--- purchase/create_an_order.php
+++ purchase/create_an_order.php
@@ -8,7 +8,6 @@ echo "Connected";
 $date = date('Y-m-d H:i:s');
 $sql = "INSERT INTO `Order` (Cost, Date, Address, Customer_ID, Payment_Info) SELECT '0', '$date', '$_POST[customer_address]', '1', '$_POST[customer_payment]' WHERE (SELECT COUNT(*) FROM Cart WHERE Customer_ID=1) > 0;";
 if ($response = $con->query($sql)){
-  echo($response);
   echo "New record is inserted sucessfully";
 }
 else{
@@ -16,6 +15,8 @@ else{
 ". $con->error;
 }
 $order_id = $con->insert_id;
+
+error_log($order_id, 0);
 
 $sql = "Update `Order` SET Cost = (SELECT SUM(Quantity * Cost) FROM Cart NATURAL JOIN Product WHERE Customer_ID=1) WHERE Order_ID = '$order_id';";
 if ($con->query($sql)){
