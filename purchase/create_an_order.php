c2edaf6875e528bd7b26d7b282f96bd12a53229b
SefusJack <46831414+SefusJack@users.noreply.github.com>
2020-11-30 16:46:42 -0600

    Revert "Update create_an_order.php"
    This reverts commit 5cba710f3064852184bf737c06b877aba3b8c92d.

--- purchase/create_an_order.php
+++ purchase/create_an_order.php
@@ -5,42 +5,60 @@ if ($con->connect_error) {
 }
 echo "Connected";
 
+if ($con->query("SELECT COUNT(*) FROM Cart WHERE Customer_ID=1")->fetch_assoc()["COUNT(*)"] <= 0) {
+  exit();
+}
+
 $date = date('Y-m-d H:i:s');
+//$sql = "INSERT INTO `Order` (Cost, Date, Address, Customer_ID, Payment_Info) SELECT '0', '$date', '$_POST[customer_address]', '1', '$_POST[customer_payment]' WHERE (SELECT COUNT(*) FROM Cart WHERE Customer_ID=1) > 0;";
 $sql = "INSERT INTO `Order` (Cost, Date, Address, Customer_ID, Payment_Info) VALUES ('0', '$date', '$_POST[customer_address]', '1', '$_POST[customer_payment]');";
+
 if ($con->query($sql)){
   echo "New record is inserted sucessfully";
 }
 else{
-  echo "Error: ". $sql ."
-". $con->error;
+  echo "Error: ". $sql . $con->error;
 }
 $order_id = $con->insert_id;
+
 
 $sql = "Update `Order` SET Cost = (SELECT SUM(Quantity * Cost) FROM Cart NATURAL JOIN Product WHERE Customer_ID=1) WHERE Order_ID = '$order_id';";
 if ($con->query($sql)){
   echo "New record is inserted sucessfully";
 }
 else{
-  echo "Error: ". $sql ."
-". $con->error;
+  echo "Error: ". $sql . $con->error;
 }
 
 
-$sql = "SELECT * FROM Cart WHERE Customer_ID=1";
+$sql = "SELECT * FROM Cart NATURAL JOIN Product WHERE Customer_ID=1";
 $result = $con->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
+      error_log("TEST", 0);
       $product_id = $row["Product_ID"];
       $quantity = $row["Quantity"];
-      $sql = "INSERT INTO Order_Products (Order_ID, Product_ID, Quantity) VALUES ('$order_id', '$product_id', '$quantity');";
+      
+      
+      $stock = $row["Stock"];
+      $newstock = $stock - $quantity;
+      $sql = "UPDATE Product SET Stock='$newstock' WHERE Product_ID=$product_id AND Customer_ID=1";
       if ($con->query($sql)){
         echo "New record is inserted sucessfully";
       }
       else{
-        echo "Error: ". $sql ."
-      ". $con->error;
+        echo "Error: ". $sql . $con->error;
+      }
+
+      $sql = "INSERT INTO Order_Products (Order_ID, Product_ID, Quantity) VALUES ($order_id, $product_id, $quantity);";
+      if ($con->query($sql)){
+        echo "New record is inserted sucessfully";
+      }
+      else{
+        echo "Error: ". $sql . $con->error;
       }
   }
 }
+
 ?>
