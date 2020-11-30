127ca887792f5336311ad187030e8b6184aa7632
Seth Williamson <sethjwilliamson@gmail.com>
2020-11-30 16:36:18 -0600

    Update create_an_order.php

--- purchase/create_an_order.php
+++ purchase/create_an_order.php
@@ -30,28 +30,34 @@ else{
   echo "Error: ". $sql . $con->error;
 }
 
-$sql = "SELECT * FROM Cart WHERE Customer_ID=1";
+
+$sql = "SELECT * FROM Cart NATURAL JOIN Product WHERE Customer_ID=1";
 $result = $con->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
+      error_log("TEST", 0);
       $product_id = $row["Product_ID"];
-      $sql = "INSERT INTO Order_Products (Order_ID, Product_ID) VALUES ('$order_id', '$product_id');";
+      $quantity = $row["Quantity"];
+      
+      
+      $stock = $row["Stock"];
+      $newstock = $stock - $quantity;
+      $sql = "UPDATE Product SET Stock='$newstock' WHERE Product_ID=$product_id AND Customer_ID=1";
       if ($con->query($sql)){
         echo "New record is inserted sucessfully";
       }
       else{
-        echo "Error: ". $sql ."
-      ". $con->error;
+        echo "Error: ". $sql . $con->error;
       }
-  }
-  $sql = "Update `Order` SET Cost = (SELECT SUM(Cost) FROM Order_Product NATURAL JOIN Product WHERE Order_ID = '$order_id') WHERE Order_ID = '$order_id';";
-  if ($con->query($sql)){
-    echo "New record is inserted sucessfully";
-  }
-  else{
-    echo "Error: ". $sql ."
-  ". $con->error;
+
+      $sql = "INSERT INTO Order_Products (Order_ID, Product_ID, Quantity) VALUES ($order_id, $product_id, $quantity);";
+      if ($con->query($sql)){
+        echo "New record is inserted sucessfully";
+      }
+      else{
+        echo "Error: ". $sql . $con->error;
+      }
   }
 }
 
