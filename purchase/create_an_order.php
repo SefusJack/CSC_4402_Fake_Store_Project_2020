9b53cd8e0491609e6e81658ea8bd837ffa098426
Seth Williamson <sethjwilliamson@gmail.com>
2020-11-30 21:33:04 -0600

    Update create_an_order.php

--- purchase/create_an_order.php
+++ purchase/create_an_order.php
@@ -10,7 +10,7 @@ if ($con->query("SELECT COUNT(*) FROM Cart WHERE Customer_ID=1")->fetch_assoc()[
 }
 
 $date = date('Y-m-d H:i:s');
-$sql = "INSERT INTO `Order` (Cost, Date, Address, Customer_ID, Payment_Info) VALUES ('0', '$date', '$_POST[customer_address]', '1', '$_POST[customer_payment]');";
+$sql = "INSERT INTO `Order` (Cost, Date, Address, Customer_ID, Payment_Info) VALUES ((SELECT SUM(Quantity * Cost) FROM Cart NATURAL JOIN Product WHERE Customer_ID=1), '$date', '$_POST[customer_address]', '1', '$_POST[customer_payment]');";
 if ($con->query($sql)){
   echo "New record is inserted sucessfully";
 }
@@ -20,14 +20,14 @@ else{
 }
 $order_id = $con->insert_id;
 
-$sql = "Update `Order` SET Cost = (SELECT SUM(Quantity * Cost) FROM Cart NATURAL JOIN Product WHERE Customer_ID=1) WHERE Order_ID = '$order_id';";
-if ($con->query($sql)){
-  echo "New record is inserted sucessfully";
-}
-else{
-  echo "Error: ". $sql ."
-". $con->error;
-}
+//$sql = "Update `Order` SET Cost = (SELECT SUM(Quantity * Cost) FROM Cart NATURAL JOIN Product WHERE Customer_ID=1) WHERE Order_ID = '$order_id';";
+//if ($con->query($sql)){
+//  echo "New record is inserted sucessfully";
+//}
+//else{
+//  echo "Error: ". $sql ."
+//". $con->error;
+//}
 
 
 $sql = "SELECT * FROM Cart WHERE Customer_ID=1";
